2ca8fd306e79656371e341fcf40c83ce559bb02a
michaelsanchez-pingidentity <michaelsanchez@pingidentity.com>
2023-03-17 13:57:00 -0700

    added bxr resources and scope

--- BXRetail-Terraform-pkg/terraform/resources.tf
+++ BXRetail-Terraform-pkg/terraform/resources.tf
@@ -82,6 +82,33 @@ resource "pingone_application_attribute_mapping" "streetAddress" {
   value = "$${user.address.streetAddress}"
 }
 
+
+
+##############################################
+# PingOne Resources & Scopes
+##############################################
+
+# @see https://registry.terraform.io/providers/pingidentity/pingone/latest/docs/resources/resource
+# @see https://docs.pingidentity.com/r/en-us/pingone/p1_c_resources
+resource "pingone_resource" "bxretail_resource" {
+  environment_id = module.environment.environment_id
+
+  name        = "BXretail"
+  description = "BXRetail sample app resource."
+
+  audience                      = "bxretail"
+  access_token_validity_seconds = 3600
+}
+
+# @see https://registry.terraform.io/providers/pingidentity/pingone/latest/docs/resources/resource_scope
+# @see https://docs.pingidentity.com/r/en-us/pingone/p1_t_addresource
+resource "pingone_resource_scope" "my_resource_scope" {
+  environment_id = module.environment.environment_id
+  resource_id    = pingone_resource.bxretail_resource.id
+
+  name = "bxretail"
+}
+
 ##############################################
 # PingOne Connections (applications)
 ##############################################

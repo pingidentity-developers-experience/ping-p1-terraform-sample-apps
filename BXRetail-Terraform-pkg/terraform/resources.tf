99293353423d3999ff2acd3feb8c5d09bcbbc0e7
michaelsanchez-pingidentity <michaelsanchez@pingidentity.com>
2023-03-22 16:37:08 -0700

    Final changes based on audit doc. 2 resources still commented out due to provider issues to be resolved.

--- BXRetail-Terraform-pkg/terraform/resources.tf
+++ BXRetail-Terraform-pkg/terraform/resources.tf
@@ -397,16 +397,82 @@ resource "pingone_notification_template_content" "pair_email_device" {
 #   email {
 #     body          = "Please use this code to reset your password. Your reset code is: $${otp}."
 #     subject       = "BXRetail: Password Reset"
-#     character_set = "UTF-8"
-#     content_type  = "text/html"
 
 #     from {
 #       name    = "BXRetail"
 #       address = "noreply@pingidentity.com"
 #     }
-#     reply_to {
+#   }
+# }
+
+# Strong authentication templates
+resource "pingone_notification_template_content" "sms_authn" {
+  environment_id = module.environment.environment_id
+  template_name  = "strong_authentication"
+  locale         = "en"
+
+  sms {
+    content = "Your BXRetail passcode is $${otp}."
+    sender  = "BXRetail"
+  }
+}
+
+resource "pingone_notification_template_content" "email_authn" {
+  environment_id = module.environment.environment_id
+  template_name  = "strong_authentication"
+  locale         = "en"
+
+  email {
+    body    = "We received a request for a one-time passcode. If youd like to signin, you can use the following one-time passcode at bxretail.org. Your passscode: $${otp}."
+    subject = "BXRetail: New Transaction Request"
+
+    from {
+      name    = "BXRetail"
+      address = "noreply@pingidentity.com"
+    }
+  }
+}
+# Transaction templates
+resource "pingone_notification_template_content" "sms_transaction" {
+  environment_id = module.environment.environment_id
+  template_name  = "transaction"
+  locale         = "en"
+
+  sms {
+    content = "Please approve this BXRetail transaction with passcode $${otp}."
+    sender  = "BXRetail"
+  }
+}
+
+resource "pingone_notification_template_content" "email_transaction" {
+  environment_id = module.environment.environment_id
+  template_name  = "transaction"
+  locale         = "en"
+
+  email {
+    body    = "Please approve this BXRetail transaction with passcode $${otp}."
+    subject = "BXFinance Transaction Request"
+
+    from {
+      name    = "BXRetail"
+      address = "noreply@pingidentity.com"
+    }
+  }
+}
+
+# Verification template
+# resource "pingone_notification_template_content" "email_verification" {
+#   environment_id = module.environment.environment_id
+#   template_name  = "verification_code_template"
+#   locale         = "en"
+
+#   email {
+#     body    = "Please use this code to verify your registration: Account verification code: $${otp}."
+#     subject = "BXRetail: Verification Code"
+
+#     from {
+#       name    = "BXRetail"
 #       address = "noreply@pingidentity.com"
-#       name    = "PingOne"
 #     }
 #   }
 # }
